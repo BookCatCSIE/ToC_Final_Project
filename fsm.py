1d627feef0b6988be1013282d9768580184be4be
BookCatCSIE <j7itgo@gmail.com>
2019-12-05 01:12:35 +0800

    Add code

--- fsm.py
+++ fsm.py
@@ -29,54 +29,6 @@ def technews():
     return content
 
 
-def ptt_beauty():
-    rs = requests.session()
-    res = rs.get('https://www.ptt.cc/bbs/Beauty/index.html', verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    all_page_url = soup.select('.btn.wide')[1]['href']
-    start_page = get_page_number(all_page_url)
-    page_term = 2  # crawler count
-    push_rate = 10  # 推文
-    index_list = []
-    article_list = []
-
-    for page in range(start_page, start_page - page_term, -1):
-        page_url = 'https://www.ptt.cc/bbs/Beauty/index{}.html'.format(page)
-        index_list.append(page_url)
-
-    # 抓取 文章標題 網址 推文數
-    while index_list:
-        index = index_list.pop(0)
-        res = rs.get(index, verify=False)
-        # 如網頁忙線中,則先將網頁加入 index_list 並休息1秒後再連接
-        if res.status_code != 200:
-            index_list.append(index)
-            # print u'error_URL:',index
-            # time.sleep(1)
-        else:
-            article_list = craw_page(res, push_rate)
-            # print u'OK_URL:', index
-            # time.sleep(0.05)
-    content = ''
-    for article in article_list:
-        data = '[{} push] {}\n{}\n\n'.format(article.get('rate', None), article.get('title', None),article.get('url', None))
-        content += data
-    return content
-
-
-def oil_price():
-    target_url = 'https://gas.goodlife.tw/'
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    title = soup.select('#main')[0].text.replace('\n', '').split('(')[0]
-    gas_price = soup.select('#gas-price')[0].text.replace('\n\n\n', '').replace(' ', '')
-    cpc = soup.select('#cpc')[0].text.replace(' ', '')
-    content = '{}\n{}{}'.format(title, gas_price, cpc)
-    return content
-
-
-
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
@@ -91,8 +43,8 @@ class TocMachine(GraphMachine):
     def is_going_to_state2(self, event):
         text = event.message.text
         #return text.lower() == "go to state2"  #檢查input text : "go to state2" ?
-        #return text.lower() == "2"
-        return text.startswith('2')
+        return text.lower() == "2"
+        #return text.startswith('2')
 
     def on_enter_state1(self, event):
         print("I'm entering state1")
@@ -168,7 +120,7 @@ class TocMachine(GraphMachine):
             send_sticker(reply_token, message)
 
         else:
-            send_text_message(reply_token, "輸入數字 :  1-1.御主抽從者  1-2.參拜者抽御神籤  1-3.隨機貼圖  2-1.爬蟲--科技新報  2-2.爬蟲--PTT表特版  2-3.爬蟲--油價 ")
+            send_text_message(reply_token, "輸入數字 :  1-1.御主抽從者  1-2.參拜者抽御神籤  1-3.隨機貼圖  2.爬蟲--科技新報 ")
 
         self.go_back()                            # state1產生output後自動回user state
 
@@ -179,23 +131,10 @@ class TocMachine(GraphMachine):
         print("I'm entering state2")
 
         reply_token = event.reply_token
-        text = event.message.text
+
+        message = technews()
+        send_text_message(reply_token, message)
         
-        if text.lower() == "2-1":
-            message = technews()
-            send_text_message(reply_token, message)
-
-        elif text.lower() == "2-2":
-            message = ptt_beauty()
-            send_text_message(reply_token, message)
-
-        elif text.lower() == "2-3":
-            message = oil_price()
-            send_text_message(reply_token, message)
-
-        else:
-            send_text_message(reply_token, "輸入數字 :  1-1.御主抽從者  1-2.參拜者抽御神籤  1-3.隨機貼圖  2-1.爬蟲--科技新報  2-2.爬蟲--PTT表特版  2-3.爬蟲--油價 ")
-
         self.go_back()                            # state2產生output後自動回user state
 
     def on_exit_state2(self):
